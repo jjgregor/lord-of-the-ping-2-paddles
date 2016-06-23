4d64d0e05f3956fd3744029aedb052031c2a7186
Jason Gregory <jagregory@zappos.com>
2016-06-22 19:13:49 -0700

    first commit

---
+++ app/src/main/java/com/android/jason/lord_of_the_ping_2_paddles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.jason.lord_of_the_ping_2_paddles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
