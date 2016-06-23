eb15c91affc46c944b20bd4dc6f3d51b898268b3
Jason Gregory <jagregory@zappos.com>
2016-06-22 20:26:56 -0700

    HoloGraph

---
+++ app/src/main/java/com/android/jason/lord_of_the_ping_2_paddles/activities/MainActivity.java
@@ -0,0 +1,111 @@
+package com.android.jason.lord_of_the_ping_2_paddles;
+
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+
+    @BindView(R.id.tabs)
+    TabLayout tabLayout;
+
+    private PingPongApplication app;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+//        EventBus.getDefault().register(this);
+
+        setSupportActionBar(toolbar);
+
+        app = (PingPongApplication) getApplication();
+
+        setUpViewPager(viewPager);
+
+        if (tabLayout != null) {
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
+    }
+
+    @OnClick(R.id.fab)
+    public void onFabClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+//        adapter.addFragment(new LeaderboardFragment(), "Leader Board");
+//        adapter.addFragment(new NewMatchFragment(), "New Match");
+//        if (app.getCurrentPlayer() == null) {
+//            adapter.addFragment(new AuthFragment(), "Profile");
+//        } else {
+//            adapter.addFragment(ProfileFragment.newInstance(app.getCurrentPlayer()), "Profile");
+//        }
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+}
